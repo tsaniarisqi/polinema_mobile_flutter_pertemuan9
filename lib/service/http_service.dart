13cf7f2365f6f8447f82b2d8e4fa2d967196d4b0
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-04-07 21:37:48 +0700

    membuat model untuk response http

--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -1,18 +1,22 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:pertemuan9/models/movie.dart';
 
 class HttpService {
   final String apiKey = '38f0a7e14a33d28494e1a5be9d8db779';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=';
 
-  Future<String> getPopularMovies() async {
+  Future<List> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("Sukses");
-      String response = result.body;
-      return response;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
       return null;
