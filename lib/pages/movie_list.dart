ed0bc92556fa2a75584d926ee7fd4b2168de86bd
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-04-07 22:41:10 +0700

    menambahkan gambar dari response api ke listview

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -41,6 +41,8 @@ class _MovieListState extends State<MovieList> {
             color: Colors.white,
             elevation: 2.0,
             child: ListTile(
+              leading: Image.network('https://image.tmdb.org/t/p/w500/' +
+                  movies[position].posterPath),
               title: Text(movies[position].title),
               subtitle: Text(
                 'Rating = ' + movies[position].voteAverage.toString(),
